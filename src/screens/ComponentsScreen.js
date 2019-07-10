45f6b16ebbb49dd96d4694de8f797c1ac4715f7d
stephen grider <ste.grider@gmail.com>
2019-07-10 15:28:51 -0600

    Err

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  const greeting = <Text>Hello to you!</Text>;
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the components screen</Text>
+      {greeting}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
