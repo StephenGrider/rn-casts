a43f448f214e431dff713cfd6a1bfd2053fde2ea
stephen grider <ste.grider@gmail.com>
2019-07-11 15:42:04 -0600

    random color

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -5,8 +5,17 @@ const ColorScreen = () => {
   return (
     <View>
       <Button title="Add a Color" />
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
 };
 
 const styles = StyleSheet.create({});
