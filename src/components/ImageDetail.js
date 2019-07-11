9bc372ed4bcd2e50e25f7d4d15e07003d6cbfd6f
stephen grider <ste.grider@gmail.com>
2019-07-11 13:55:59 -0600

    images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = props => {
-  return <Text>{props.title}</Text>;
+  return (
+    <View>
+      <Image source={require('../../assets/beach.jpg')} />
+      <Text>{props.title}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
