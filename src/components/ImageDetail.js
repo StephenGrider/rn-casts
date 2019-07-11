0a0fefd8d264689ce4c0c6a7d09e64abf90d17ef
stephen grider <ste.grider@gmail.com>
2019-07-11 14:13:37 -0600

    solution

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = props => {
+const ImageDetail = ({ imageSource, title, score }) => {
   return (
     <View>
-      <Image source={props.imageSource} />
-      <Text>{props.title}</Text>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image Score - {score}</Text>
     </View>
   );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -8,14 +8,17 @@ const ImageScreen = () => {
       <ImageDetail
         title="Forest"
         imageSource={require('../../assets/forest.jpg')}
+        score={9}
       />
       <ImageDetail
         title="Beach"
         imageSource={require('../../assets/beach.jpg')}
+        score={7}
       />
       <ImageDetail
         title="Mountain"
         imageSource={require('../../assets/mountain.jpg')}
+        score={10}
       />
     </View>
   );
