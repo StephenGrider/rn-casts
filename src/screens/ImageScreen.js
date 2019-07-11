35e8b607def72ba938eef825225c4664b133265e
stephen grider <ste.grider@gmail.com>
2019-07-11 13:33:39 -0600

    props

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageScreen = () => {
+  return <Text>Image Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
