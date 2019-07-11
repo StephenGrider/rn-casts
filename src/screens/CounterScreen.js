1641d799f5ec681e7a174c085969af7a85310771
stephen grider <ste.grider@gmail.com>
2019-07-11 15:09:37 -0600

    state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          // Don't do this!
+          // counter++;
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   );
 };
