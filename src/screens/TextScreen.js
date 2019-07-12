430d3f6c60111aba483d451f9fb9fea6d63150f7
stephen grider <ste.grider@gmail.com>
2019-07-12 15:16:42 -0600

    two

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -4,7 +4,11 @@ import { View, Text, StyleSheet, TextInput } from 'react-native';
 const TextScreen = () => {
   return (
     <View>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
     </View>
   );
 };
