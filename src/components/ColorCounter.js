d88c9c6bd9ade080a6d00544431c6616bb83a988
stephen grider <ste.grider@gmail.com>
2019-07-12 11:29:24 -0600

    passing

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`} />
-      <Button title={`Decrease ${color}`} />
+      <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
+      <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
     </View>
   );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View>
-      <ColorCounter color="Red" />
+      <ColorCounter
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+        color="Red"
+      />
       <ColorCounter color="Blue" />
       <ColorCounter color="Green" />
     </View>
