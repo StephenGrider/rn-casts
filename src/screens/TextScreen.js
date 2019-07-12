3d6fe04539771cf77b653debf73bc47551b27568
stephen grider <ste.grider@gmail.com>
2019-07-12 15:11:56 -0600

    styling

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>Text Screen</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
