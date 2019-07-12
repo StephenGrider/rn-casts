3253023bd8a8b00179776b0e58130f0ab7893e9e
stephen grider <ste.grider@gmail.com>
2019-07-12 15:32:32 -0600

    update

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
+
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={newValue => setName(newValue)}
       />
+      <Text>My name is {name}</Text>
     </View>
   );
 };
