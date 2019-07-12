3d026046e92b517a492c88181f06b48b1254e225
stephen grider <ste.grider@gmail.com>
2019-07-12 11:58:43 -0600

    validating

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,11 +9,24 @@ const SquareScreen = () => {
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const setColor = (color, change) => {
+    // color === 'red', 'green', 'blue'
+    // change === +15, -15
+
+    if (color === 'red') {
+      if (red + change > 255 || red + change < 0) {
+        return;
+      } else {
+        setRed(red + change);
+      }
+    }
+  };
+
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
         color="Red"
       />
       <ColorCounter
