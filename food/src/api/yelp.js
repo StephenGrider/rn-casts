58b3a68fdfa2fe96c42675a97a42af045b3de71c
stephen grider <ste.grider@gmail.com>
2019-07-15 16:58:08 -0600

    config axios

---
+++ food/src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization:
+      'Bearer l2cmhf2ezRl6ZkcHWNYKoDpiaro1zlqUjwkA7nVxnWzryiTwFDk35PJiucoLhjjFY9ECD8GTBGaHBg5yv5YDLiszKQx8EMvm30ply0UWoHQOnYFjLozpYnZOx-UsXXYx'
+  }
+});
