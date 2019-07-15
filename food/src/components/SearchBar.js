850ffa59caeeafe1bfddbe17382f1ef66e585c74
stephen grider <ste.grider@gmail.com>
2019-07-15 16:40:32 -0600

    manage state

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+      />
     </View>
   );
 };
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
