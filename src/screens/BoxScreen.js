9cec363e9e20acc70a553af3270b30724ed209c5
stephen grider <ste.grider@gmail.com>
2019-07-15 11:54:31 -0600

    box object model

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,22 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Box Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black'
+  },
+  textStyle: {
+    borderWidth: 10,
+    borderColor: 'red',
+    margin: 20
+  }
+});
 
 export default BoxScreen;
