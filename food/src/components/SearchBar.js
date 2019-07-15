8ce356b79a484975cf6638e4192de47f32fc1d15
stephen grider <ste.grider@gmail.com>
2019-07-15 16:13:19 -0600

    styling

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
+    <View style={styles.backgroundStyle}>
       <Feather name="search" size={30} />
-      <Text>Search Bar</Text>
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 1
   }
 });
 
