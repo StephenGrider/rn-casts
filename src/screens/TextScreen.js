1a3454eef75d76c95992d61e0d3b0751251ca948
stephen grider <ste.grider@gmail.com>
2019-07-15 11:24:51 -0600

    solution

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <View>
-      <Text>Enter Name:</Text>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        onChangeText={newValue => setName(newValue)}
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}
       />
-      <Text>My name is {name}</Text>
+      {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
     </View>
   );
 };
