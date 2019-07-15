5e3993fe55321d4e4a5fedb1decca4c5d3eee84f
stephen grider <ste.grider@gmail.com>
2019-07-15 16:18:22 -0600

    styling

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -5,7 +5,7 @@ import { Feather } from '@expo/vector-icons';
 const SearchBar = () => {
   return (
     <View style={styles.backgroundStyle}>
-      <Feather name="search" size={30} />
+      <Feather name="search" style={styles.iconStyle} />
       <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
@@ -13,6 +13,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 10,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
@@ -20,9 +21,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
-    flex: 1
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
   }
 });
 
