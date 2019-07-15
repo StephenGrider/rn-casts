d51ccd13958364e2021abdee0dc684e49603570f
stephen grider <ste.grider@gmail.com>
2019-07-15 15:36:35 -0600

    create navigator

--- food/App.js
+++ food/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import SearchScreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
   },
-});
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
