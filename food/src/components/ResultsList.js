91b9af854af2316fcd5bd5ddb8e740c1eef5190e
stephen grider <ste.grider@gmail.com>
2019-07-16 15:35:47 -0600

    last fix

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -10,6 +10,10 @@ import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
