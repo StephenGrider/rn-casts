0de148a42eee1f24454b182c737c4459cbe9f89c
stephen grider <ste.grider@gmail.com>
2019-07-16 12:39:09 -0600

    additional

--- food/src/components/ResultsDetail.js
+++ food/src/components/ResultsDetail.js
@@ -4,11 +4,24 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 const ResultsDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {
+    fontWeight: 'bold'
+  }
+});
 
 export default ResultsDetail;
