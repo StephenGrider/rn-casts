1eefc03d948501096d3dcedf33495f7cf5a17489
stephen grider <ste.grider@gmail.com>
2019-07-16 12:20:18 -0600

    Grouping

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ResultsList = ({ title }) => {
+const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
+      <Text>Results: {results.length}</Text>
     </View>
   );
 };
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -8,6 +8,13 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
+
   return (
     <View>
       <SearchBar
@@ -17,9 +24,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title="Cost Effective" />
-      <ResultsList title="Bit Pricier" />
-      <ResultsList title="Big Spender" />
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
