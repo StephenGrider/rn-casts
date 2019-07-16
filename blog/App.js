97da739697f0a2d3e9002ccc8d13ab6502906e18
stephen grider <ste.grider@gmail.com>
2019-07-16 17:09:09 -0600

    context

---
+++ blog/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
+  },
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
---
+++ blog/src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
