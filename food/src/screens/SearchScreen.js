84525f9090a8114aacec259555887da7c1b95441
stephen grider <ste.grider@gmail.com>
2019-07-16 11:46:57 -0600

    usestate

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -27,6 +27,9 @@ const SearchScreen = () => {
   // Call searchApi when component
   // is first rendered.  BAD CODE!
   // searchApi('pasta');
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View>
