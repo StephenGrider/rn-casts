b1034943e8eaf93b430e705d3fcd88c73981f276
stephen grider <ste.grider@gmail.com>
2019-07-16 12:25:21 -0600

    flatlist

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={result => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
