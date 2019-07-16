eb7508cb2acc28ed7d05dda2a72bb11e210b32a9
stephen grider <ste.grider@gmail.com>
2019-07-16 11:55:03 -0600

    extract hook logic

---
+++ food/src/hooks/useResults.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async searchTerm => {
+    console.log('Hi there!');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  // Call searchApi when component
+  // is first rendered.  BAD CODE!
+  // searchApi('pasta');
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,35 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async searchTerm => {
-    console.log('Hi there!');
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  // Call searchApi when component
-  // is first rendered.  BAD CODE!
-  // searchApi('pasta');
-  useEffect(() => {
-    searchApi('pasta');
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
