c91e2913adee50b492641c0eaaf5dcddd50f3613
stephen grider <ste.grider@gmail.com>
2019-07-16 11:42:05 -0600

    initial

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -8,12 +8,13 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async searchTerm => {
+    console.log('Hi there!');
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'san jose'
         }
       });
@@ -23,9 +24,17 @@ const SearchScreen = () => {
     }
   };
 
+  // Call searchApi when component
+  // is first rendered.  BAD CODE!
+  // searchApi('pasta');
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
