e6e8ae525ca8d740d7a02ec8bf7f2af6276f6aa1
stephen grider <ste.grider@gmail.com>
2019-07-17 14:31:09 -0600

    delete

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,7 +3,13 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`
+        }
+      ];
     default:
       return state;
   }
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,5 +1,12 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity
+} from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
@@ -15,8 +22,12 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return (
             <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Feather style={styles.icon} name="trash" />
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => console.log(item.id)}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
             </View>
           );
         }}
