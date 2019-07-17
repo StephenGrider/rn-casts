89e46c43a38feb822d2234d561d442ab2b75ad53
stephen grider <ste.grider@gmail.com>
2019-07-17 11:23:45 -0600

    list

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,16 +1,21 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
   const { data, addBlogPost } = useContext(BlogContext);
 
-  console.log(data);
-
   return (
     <View>
       <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blog => blog.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
