8306d74ffbe7f7cddd4fef89416265c043f183f0
stephen grider <ste.grider@gmail.com>
2019-07-17 12:51:53 -0600

    usereducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` }
-    ]);
+    dispatch({ type: 'add_blogpost' });
   };
 
   return (
