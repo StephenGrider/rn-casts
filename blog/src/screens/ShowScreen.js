4b49cf3713eb6126d05d194c2b4397f82cef41a6
stephen grider <ste.grider@gmail.com>
2019-07-17 16:12:27 -0600

    icon

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -12,8 +13,19 @@ const ShowScreen = ({ navigation }) => {
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    )
+  };
 };
 
 const styles = StyleSheet.create({});
