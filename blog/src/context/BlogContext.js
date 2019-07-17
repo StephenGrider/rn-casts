42cecd90042d515d2ef35691a060c640c63590a9
stephen grider <ste.grider@gmail.com>
2019-07-17 12:18:41 -0600

    movingdata

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,5 +3,9 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  return (
+    <BlogContext.Provider value={[1, 2, 3]}>{children}</BlogContext.Provider>
+  );
 };
+
+export default BlogContext;
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+  const value = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <Text>{value}</Text>
     </View>
   );
 };
