3227ecf1aa48bab4c3553b6a9a52886dbdeefd72
stephen grider <ste.grider@gmail.com>
2019-07-17 12:23:21 -0600

    renderlist

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,8 +3,10 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const blogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }];
+
   return (
-    <BlogContext.Provider value={[1, 2, 3]}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
   );
 };
 
--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
 
   return (
     <View>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
