d39a1517af8dc54c0ec08f382696e553c116e8dd
stephen grider <ste.grider@gmail.com>
2019-07-17 14:57:00 -0600

    retrieve

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const ShowScreen = () => {
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam('id')
+  );
+
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
