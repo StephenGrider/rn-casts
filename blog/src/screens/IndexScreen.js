b1f3b3954d702aa6fdb79ec781154d7adaab8c11
stephen grider <ste.grider@gmail.com>
2019-07-18 16:18:17 -0600

    refetch

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -15,6 +15,14 @@ const IndexScreen = ({ navigation }) => {
 
   useEffect(() => {
     getBlogPosts();
+
+    const listener = navigation.addListener('didFocus', () => {
+      getBlogPosts();
+    });
+
+    return () => {
+      listener.remove();
+    };
   }, []);
 
   return (
