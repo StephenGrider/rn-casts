0c52a3c3543821a44547e944fb9dc3e2167e1dda
stephen grider <ste.grider@gmail.com>
2019-07-18 16:24:07 -0600

    delete

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -11,15 +11,6 @@ const blogReducer = (state, action) => {
       });
     case 'delete_blogpost':
       return state.filter(blogPost => blogPost.id !== action.payload);
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
     default:
       return state;
   }
@@ -37,19 +28,22 @@ const addBlogPost = dispatch => {
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', { title, content });
 
-    // dispatch({ type: 'add_blogpost', payload: { title, content } });
     if (callback) {
       callback();
     }
   };
 };
+
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
+
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
+
 const editBlogPost = dispatch => {
-  return (id, title, content) => {
+  return (id, title, content, callback) => {
     dispatch({
       type: 'edit_blogpost',
       payload: { id, title, content }
