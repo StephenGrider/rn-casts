61fcd936ebd99e36759a0151e08e17f3ca6b5692
stephen grider <ste.grider@gmail.com>
2019-07-18 16:08:33 -0600

    creating

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -34,8 +34,10 @@ const getBlogPosts = dispatch => {
 };
 
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
+
+    // dispatch({ type: 'add_blogpost', payload: { title, content } });
     if (callback) {
       callback();
     }
