fd837e49284503334ccc4ce821a58ebd09bcf428
stephen grider <ste.grider@gmail.com>
2019-07-18 16:33:52 -0600

    edit

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -43,7 +43,9 @@ const deleteBlogPost = dispatch => {
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({
       type: 'edit_blogpost',
       payload: { id, title, content }
