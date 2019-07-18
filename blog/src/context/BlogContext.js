e1dcc02aae88ab725109aba6ec92a345f3d49162
stephen grider <ste.grider@gmail.com>
2019-07-18 12:01:22 -0600

    data fetching

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,14 +1,16 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+      return action.payload;
     case 'delete_blogpost':
       return state.filter(blogPost => blogPost.id !== action.payload);
     case 'add_blogpost':
       return [
         ...state,
         {
-          id: Math.floor(Math.random() * 99999),
           title: action.payload.title,
           content: action.payload.content
         }
@@ -29,9 +31,16 @@ const deleteBlogPost = dispatch => {
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
+const fetchBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+    // response.data === [{ title: 'alsdf', content: 'asdlfkj', id: 1}]
+    dispatch({ type: 'get_blogposts', payload: response.data });
+  };
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+  { addBlogPost, deleteBlogPost, fetchBlogPosts },
+  []
 );
