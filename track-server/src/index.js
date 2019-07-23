cf3e0261721b65c07cd0fcc595d5aecdaccce746
stephen grider <ste.grider@gmail.com>
2019-07-23 16:08:23 -0600

    bodyparser

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri =
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
+  console.log(req.body);
   res.send('You made a post request');
 });
 
