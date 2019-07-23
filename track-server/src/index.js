f1c20b3a2ffc374b61cfa191e62580cac8a8baa5
stephen grider <ste.grider@gmail.com>
2019-07-23 14:05:30 -0600

    express app

---
+++ track-server/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
