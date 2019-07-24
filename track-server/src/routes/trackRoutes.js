b719e86c81d8457cca454d6d6c8417c4bde1ad2e
stephen grider <ste.grider@gmail.com>
2019-07-24 14:51:47 -0600

    Create tracks

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -14,4 +14,22 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks);
 });
 
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
 module.exports = router;
