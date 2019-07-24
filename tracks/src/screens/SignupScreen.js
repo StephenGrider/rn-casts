6323859d66fe7fa78c69a7aa78583b5b7284e9ae
stephen grider <ste.grider@gmail.com>
2019-07-24 16:08:22 -0600

    nav

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button
+        title="Go to Signin"
+        onPress={() => navigation.navigate('Signin')}
+      />
+      <Button
+        title="Go to main flow"
+        onPress={() => navigation.navigate('mainFlow')}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackListScreen</Text>;
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => navigation.navigate('TrackDetail')}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
