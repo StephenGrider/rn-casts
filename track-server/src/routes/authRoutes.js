d9904525d095e0647d9148137c0fb3b776721113
stephen grider <ste.grider@gmail.com>
2019-07-24 12:32:59 -0600

    signin

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -19,4 +19,25 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
+});
+
 module.exports = router;
