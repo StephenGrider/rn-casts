5880ea152d8cd49d5abbd213f7c6c6f79a6475c3
stephen grider <ste.grider@gmail.com>
2019-07-24 12:23:28 -0600

    Salt hash

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -11,5 +12,40 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function comparePassword(
+  candidatePassword
+) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(true);
+      }
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
