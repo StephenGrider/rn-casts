ac5138d98833432f42b97b32ef65628d2c676455
stephen grider <ste.grider@gmail.com>
2019-07-24 14:35:02 -0600

    listing

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri =
   'mongodb+srv://admin:passwordpassword@cluster0-8fzga.mongodb.net/test?retryWrites=true&w=majority';
---
+++ track-server/src/routes/trackRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireAuth = require('../middlewares/requireAuth');
+
+const Track = mongoose.model('Track');
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get('/tracks', async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
+
+  res.send(tracks);
+});
+
+module.exports = router;
