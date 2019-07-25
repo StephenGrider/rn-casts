b687b1d5dd3ac77d2f34cb1e779d8049bfa34f39
stephen grider <ste.grider@gmail.com>
2019-07-25 12:39:58 -0600

    handle errored request

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -3,6 +3,8 @@ import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
@@ -14,7 +16,10 @@ const signup = dispatch => {
       const response = await trackerApi.post('/signup', { email, password });
       console.log(response.data);
     } catch (err) {
-      console.log(err.response.data);
+      dispatch({
+        type: 'add_error',
+        payload: 'Something went wrong with sign up'
+      });
     }
   };
 };
@@ -36,5 +41,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: '' }
 );
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -30,6 +30,9 @@ const SignupScreen = ({ navigation }) => {
         autoCapitalize="none"
         autoCorrect={false}
       />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
       <Spacer>
         <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
@@ -48,6 +51,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15
   }
 });
 
