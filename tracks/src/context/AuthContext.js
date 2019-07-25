dd104d761d6d2e5d145883bec9134915875cc061
stephen grider <ste.grider@gmail.com>
2019-07-25 11:44:43 -0600

    context

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,8 +7,31 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = dispatch => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we sign up, modify our state, and say that we are authenticated
+    // if signing up fails, we probably need to reflect an error message
+    // somewhere
+  };
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    // somehow sign out!!!
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
