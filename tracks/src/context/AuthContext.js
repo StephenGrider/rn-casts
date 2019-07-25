ab0e84dd1ecfd23c1311dfeba3c539300fca5508
stephen grider <ste.grider@gmail.com>
2019-07-25 14:37:55 -0600

    typeo

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -18,7 +18,7 @@ const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
-    dispatch({ type: 'signup', payload: reponse.data.token });
+    dispatch({ type: 'signup', payload: response.data.token });
 
     navigate('TrackList');
   } catch (err) {
