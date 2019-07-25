3bec103d52bacbc24f5333c0f40ef63c9996f15b
stephen grider <ste.grider@gmail.com>
2019-07-25 12:31:15 -0600

    api

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -8,11 +9,13 @@ const authReducer = (state, action) => {
 };
 
 const signup = dispatch => {
-  return ({ email, password }) => {
-    // make api request to sign up with that email and password
-    // if we sign up, modify our state, and say that we are authenticated
-    // if signing up fails, we probably need to reflect an error message
-    // somewhere
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
   };
 };
 
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -29,7 +31,7 @@ const SignupScreen = ({ navigation }) => {
         autoCorrect={false}
       />
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
