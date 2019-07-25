e6083d6dfb36ef775eea7fb0965e106e9b2cd0a8
stephen grider <ste.grider@gmail.com>
2019-07-25 15:12:15 -0600

    Reuse

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        text="Dont have an account? Sign up instead"
+        routeName="Signup"
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = {
+  header: null
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SigninScreen;
