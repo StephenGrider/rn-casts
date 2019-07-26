eb0e92df85b7ed1524b067383ac7d7ba2ba94cbf
stephen grider <ste.grider@gmail.com>
2019-07-26 12:05:44 -0600

    signout

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
-  return <Text style={{ fontSize: 48 }}>AccountScreen</Text>;
+  const { signout } = useContext(AuthContext);
+
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
