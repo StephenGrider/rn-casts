285148101fa70882e1862a7bb9a58bcb10c96eb6
stephen grider <ste.grider@gmail.com>
2019-07-30 14:35:08 -0600

    conditional

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -9,7 +9,7 @@ import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
