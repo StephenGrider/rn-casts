c855c7924d6d0c5946df468d177e396bebc3f0e5
stephen grider <ste.grider@gmail.com>
2019-07-30 12:29:41 -0600

    indicator

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text, StyleSheet, ActivityIndicator } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -20,12 +20,14 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
-      }}
-    />
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+    </MapView>
   );
 };
 
