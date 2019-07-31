4313646b21312d263e6966383db660123c1933fb
stephen grider <ste.grider@gmail.com>
2019-07-31 15:28:56 -0600

    form reset

--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -12,6 +12,8 @@ const locationReducer = (state, action) => {
       return { ...state, locations: [...state.locations, action.payload] };
     case 'change_name':
       return { ...state, name: action.payload };
+    case 'reset':
+      return { ...state, name: '', locations: [] };
     default:
       return state;
   }
@@ -32,9 +34,12 @@ const addLocation = dispatch => (location, recording) => {
     dispatch({ type: 'add_location', payload: location });
   }
 };
+const reset = dispatch => () => {
+  dispatch({ type: 'reset' });
+};
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation, changeName },
+  { startRecording, stopRecording, addLocation, changeName, reset },
   { name: '', recording: false, locations: [], currentLocation: null }
 );
--- tracks/src/context/TrackContext.js
+++ tracks/src/context/TrackContext.js
@@ -9,9 +9,8 @@ const trackReducer = (state, action) => {
 };
 
 const fetchTracks = dispatch => () => {};
-const createTrack = dispatch => (name, locations) => {
-  // make a request to our api
-  console.log(name, locations.length);
+const createTrack = dispatch => async (name, locations) => {
+  await trackerApi.post('/tracks', { name, locations });
 };
 
 export const { Provider, Context } = createDataContext(
