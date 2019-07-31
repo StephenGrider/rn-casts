e4c06bf9c62d176d306a1e66fe4209e686f4e883
stephen grider <ste.grider@gmail.com>
2019-07-31 15:50:51 -0600

    navigating

--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackDetailScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>;
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam('_id');
+
+  const track = state.find(t => t._id === _id);
+
+  return <Text style={{ fontSize: 48 }}>{track.name}</Text>;
 };
 
 const styles = StyleSheet.create({});
