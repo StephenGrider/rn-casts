88ed09d9402506e11849593756c02e9c1163ba0c
stephen grider <ste.grider@gmail.com>
2019-07-31 15:34:35 -0600

    fetching

--- tracks/src/context/TrackContext.js
+++ tracks/src/context/TrackContext.js
@@ -3,12 +3,17 @@ import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case 'fetch_tracks':
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+  const response = await trackerApi.get('/tracks');
+  dispatch({ type: 'fetch_tracks', payload: response.data });
+};
 const createTrack = dispatch => async (name, locations) => {
   await trackerApi.post('/tracks', { name, locations });
 };
