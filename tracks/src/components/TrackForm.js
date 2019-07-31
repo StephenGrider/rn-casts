cb116796a755f408614b0ec40d27ab1414b64147
stephen grider <ste.grider@gmail.com>
2019-07-31 15:09:10 -0600

    coordination

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -10,6 +11,7 @@ const TrackForm = () => {
     stopRecording,
     changeName
   } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack();
 
   return (
     <>
@@ -29,7 +31,7 @@ const TrackForm = () => {
       </Spacer>
       <Spacer>
         {!recording && locations.length ? (
-          <Button title="Save Recording" />
+          <Button title="Save Recording" onPress={saveTrack} />
         ) : null}
       </Spacer>
     </>
