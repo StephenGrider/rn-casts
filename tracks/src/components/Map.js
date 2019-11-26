ecea851792ec485960f7503cffa83b81c846413a
stephen grider <ste.grider@gmail.com>
2019-11-26 14:37:34 -0600

    points

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03121 + i * 0.001
+      });
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 + i * 0.001
+      });
+    }
+  }
+
   return (
     <MapView
       style={styles.map}
@@ -12,7 +27,9 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-    />
+    >
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
