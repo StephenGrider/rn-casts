bebbb8b8eee20374911fd6c154f182bf2e423a34
stephen grider <ste.grider@gmail.com>
2019-11-30 16:32:20 -0600

    updates

--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -4,20 +4,37 @@ const locationReducer = (state, action) => {
   switch (action.type) {
     case 'add_current_location':
       return { ...state, currentLocation: action.payload };
+    case 'start_recording':
+      return { ...state, recording: true };
+    case 'stop_recording':
+      return { ...state, recording: false };
+    case 'add_location':
+      return { ...state, locations: [...state.locations, action.payload] };
+    case 'change_name':
+      return { ...state, name: action.payload };
     default:
       return state;
   }
 };
 
-const startRecording = dispatch => () => {};
-const stopRecording = dispatch => () => {};
-const addLocation = dispatch => location => {
-  console.log('HI THERE!');
+const changeName = dispatch => name => {
+  dispatch({ type: 'change_name', payload: name });
+};
+const startRecording = dispatch => () => {
+  dispatch({ type: 'start_recording' });
+};
+const stopRecording = dispatch => () => {
+  dispatch({ type: 'stop_recording' });
+};
+const addLocation = dispatch => (location, recording) => {
   dispatch({ type: 'add_current_location', payload: location });
+  if (recording) {
+    dispatch({ type: 'add_location', payload: location });
+  }
 };
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation },
-  { recording: false, locations: [], currentLocation: null }
+  { startRecording, stopRecording, addLocation, changeName },
+  { name: '', recording: false, locations: [], currentLocation: null }
 );
