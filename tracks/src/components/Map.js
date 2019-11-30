5f6ff85745532b0f4477ba12f2ec5e93a2ba1b03
stephen grider <ste.grider@gmail.com>
2019-11-30 11:58:35 -0600

    update

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, ActivityIndicator } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      points.push({
-        latitude: 37.33233 + i * 0.001,
-        longitude: -122.03121 + i * 0.001
-      });
-    } else {
-      points.push({
-        latitude: 37.33233 - i * 0.002,
-        longitude: -122.03121 + i * 0.001
-      });
-    }
+  const {
+    state: { currentLocation }
+  } = useContext(LocationContext);
+
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
 
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+        ...currentLocation.coords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapView>
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
   );
 };
 
--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const locationReducer = (state, action) => {
   switch (action.type) {
+    case 'add_current_location':
+      return { ...state, currentLocation: action.payload };
     default:
       return state;
   }
@@ -9,7 +11,9 @@ const locationReducer = (state, action) => {
 
 const startRecording = dispatch => () => {};
 const stopRecording = dispatch => () => {};
-const addLocation = dispatch => () => {};
+const addLocation = dispatch => location => {
+  dispatch({ type: 'add_current_location', payload: location });
+};
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
