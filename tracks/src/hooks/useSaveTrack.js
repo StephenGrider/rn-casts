3c124e88867cf9b1882e178993b2f61f3036e0f5
stephen grider <ste.grider@gmail.com>
2019-12-01 15:28:56 -0600

    form reset

--- tracks/src/hooks/useSaveTrack.js
+++ tracks/src/hooks/useSaveTrack.js
@@ -1,15 +1,19 @@
 import { useContext } from 'react';
 import { Context as TrackContext } from '../context/TrackContext';
 import { Context as LocationContext } from '../context/LocationContext';
+import { navigate } from '../navigationRef';
 
 export default () => {
   const { createTrack } = useContext(TrackContext);
   const {
-    state: { locations, name }
+    state: { locations, name },
+    reset
   } = useContext(LocationContext);
 
-  const saveTrack = () => {
-    createTrack(name, locations);
+  const saveTrack = async () => {
+    await createTrack(name, locations);
+    reset();
+    navigate('TrackList');
   };
 
   return [saveTrack];
