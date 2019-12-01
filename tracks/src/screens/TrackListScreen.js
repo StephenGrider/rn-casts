718dac96adcec262f03b5952458fd09195d2b0ca
stephen grider <ste.grider@gmail.com>
2019-12-01 15:50:51 -0600

    navigating

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -16,7 +16,11 @@ const TrackListScreen = ({ navigation }) => {
         keyExtractor={item => item._id}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('TrackDetail', { _id: item._id })
+              }
+            >
               <ListItem chevron title={item.name} />
             </TouchableOpacity>
           );
