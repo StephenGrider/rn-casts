8436c6f0e815edfc60bb8f02fac12fc557ded2d2
stephen grider <ste.grider@gmail.com>
2019-12-01 15:20:24 -0600

    automatic

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
-  baseURL: 'http://5c44e3d9.ngrok.io'
+const instance = axios.create({
+  baseURL: 'http://cd14184c.ngrok.io'
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
