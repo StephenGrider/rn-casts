03f6074b0519bba20bdcb62b843bd2b6955e125d
stephen grider <ste.grider@gmail.com>
2019-12-01 14:52:21 -0600

    readd polyline

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -5,7 +5,7 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
   const {
-    state: { currentLocation }
+    state: { currentLocation, locations }
   } = useContext(LocationContext);
 
   if (!currentLocation) {
@@ -27,6 +27,7 @@ const Map = () => {
         strokeColor="rgba(158, 158, 255, 1.0)"
         fillColor="rgba(158, 158, 255, 0.3)"
       />
+      <Polyline coordinates={locations.map(loc => loc.coords)} />
     </MapView>
   );
 };
