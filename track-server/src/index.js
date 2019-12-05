1b7b767ea3af2c768ecc2312c3df4bc8ec6de0b2
stephen grider <ste.grider@gmail.com>
2019-12-05 11:13:22 -0600

    fixing

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -30,6 +30,7 @@ app.get('/', requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
