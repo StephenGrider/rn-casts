c5676afe7222c45e007c22c07d3e1c5b68e0f09f
stephen grider <ste.grider@gmail.com>
2019-12-05 11:13:22 -0600

    fixing

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
+let url;
+if (__DEV__) {
+  url = 'http://cd14184c.ngrok.io';
+} else {
+  url = 'https://sleepy-savannah-10606.herokuapp.com';
+}
+
 const instance = axios.create({
-  baseURL: 'http://cd14184c.ngrok.io'
+  baseURL: url
 });
 
 instance.interceptors.request.use(
