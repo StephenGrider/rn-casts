5c39af28085ac9fddd5c774d349c973fbefc2216
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-15 14:12:05 +0200

    changed the title

--- food/App.js
+++ food/App.js
@@ -10,7 +10,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Search',
     defaultNavigationOptions: {
-      title: 'Business Search'
+      title: 'update from kelly'
     }
   }
 );
